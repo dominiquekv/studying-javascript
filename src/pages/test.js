7529b1cc24474a4bbe81f216dd64b57d3fdd8f86
Dominique Vieira <dominiquekv18@gmail.com>
2022-06-19 16:08:12 -0300

    test.js

---
+++ src/pages/test.js
@@ -0,0 +1,16 @@
+function returnEvenValues(array) {
+
+    let evenNums = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array [i] % 2 === 0) {
+            evenNums.push(array[i]);
+        } else {
+            console.log(`${array[i]} is not even.`);
+        }
+    }
+    console.log('The even numbers are: ' + evenNums);
+}
+
+let array = [1, 2, 3, 4, 5, 8];
+
+returnEvenValues(array);
